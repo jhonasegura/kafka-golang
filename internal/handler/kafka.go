2965b59c07431c7c9bd2ef11bc34cf56e73d1e61
Jhonathan Salas <jhonssegura@outlook.com>
2022-05-28 10:13:18 -0500

    Actualizando archivo kafka

--- internal/handler/kafka.go
+++ internal/handler/kafka.go
@@ -1,5 +1,29 @@
 package handler
 
-func MakeConsumerBuilder() {
+import (
+	"context"
+	"fmt"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func StartKafka() {
+	conf := kafka.ReaderConfig{
+		Brokers:  []string{"localhost:9882"},
+		Topic:    "mytopic",
+		GroupID:  "g1",
+		MaxBytes: 18,
+	}
+
+	reader := kafka.NewReader(conf)
+
+	for {
+		m, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			fmt.Println("Some error occurred", err)
+			continue
+		}
+		fmt.Println("Message is: ", string(m.Value))
+	}
 
 }
